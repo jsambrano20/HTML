d9528c7f900fc5794c7b5499bd4414f769697495
João Sambrano <92763436+jsambrano20@users.noreply.github.com>
2021-11-03 07:49:32 -0300

    Add files via upload
    JAVA inicial HTML

---
+++ JAVA/arquivo.js
@@ -0,0 +1,72 @@
+
+// var n1 = 10;
+// var n2 = 10.5;
+// var n3 = 1.5e3;
+// var n4 = 0xfff;
+
+// var t1 = "teste";
+// t1 = n2;
+
+// var b1 =  true;
+
+// var objeto =
+// {
+//     nome = "EU",
+//     cargo = "Professor"
+// }
+
+// var array = ["c#", "CSS" , "HTML", "JS"];
+
+
+// var numero = 1;
+// if (numero == '1') { //compara os valores/conteudos "=="
+//     alert('True')
+
+// }
+// else {
+//     alert('False')
+// }
+
+// if (numero === 1) { //compara o tipo e o valor(ambos dever ser iguais) "==="
+//     alert('True')
+
+// }
+// else {
+//     alert('False')
+// }
+
+
+// --------------------------------
+// var suco = 3;
+
+// switch (suco){
+//     case 1:
+//     alert ('Um como de suco');
+//     break;
+//     case 2:
+//   alert ('Dois copos');
+//     break;    
+//     case 2:
+//         alert ('tres copos');
+//           break;    
+
+// }
+// -----------------------------------
+// var alunos = ["Joao" , "Sambrano", "Victor"]
+
+// for(let i = 0; i< Array.length; i++)
+// {
+// alert(alunos[i]);
+// }
+
+// ------------------------------------------
+
+
+var texto = "";
+var numero = 1;
+
+while (numero < 10) {
+    texto += numero + "";
+    numero++;
+}
+alert(texto);
